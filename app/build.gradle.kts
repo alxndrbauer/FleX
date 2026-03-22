2bafa1d4049d26f4ae17b207fb67087298592113
Alexander Bauer <contact@alexander-bauer.de>
2026-03-22 20:35:03 +0100

    bump version to 1.4.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.4.1"
+        versionName = "1.4.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
